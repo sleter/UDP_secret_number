25c27acbd4c98a45d22583cec453a6c0a74f46b4
Szymon Janowski <szy.janowski@gmail.com>
2017-12-03 19:08:56 +0100

    final

---
+++ client_o.py
@@ -0,0 +1,109 @@
+import socket
+import time
+import re
+import random
+
+class packet:
+    host = '127.0.0.1'
+    port = random.randrange(5000,20000) #random not used port
+
+    server = ('127.0.0.1', 5678) #localhost + server port
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.bind((host, port))
+    except:
+        port +=1
+        s.bind((host, port))
+
+    L = 0
+    max = 0
+    czy_ktos_zgadl = False
+    #-------------------------------------------------------
+    operacja = 0
+    czas = ''
+    id = ''
+    odpowiedz = ''
+    final_packet = ''
+    ack = ''
+
+    #-------------------------------------------------------
+    def stworz_pakiet(self):
+        self.final_packet = "Czas>"+self.czas+"<Identyfikator>"+self.id+"<Operacja>"+str(self.operacja)+"<Odpowiedz>"+self.odpowiedz+"<Ack>"+self.ack+"<"
+
+    def decode_packet(self):
+        data, addr = self.s.recvfrom(1024)
+        data = data.decode('utf-8')
+        result = re.findall('>(.*?)<', data)
+        self.czas = result[0]
+        self.id = result[1]
+        self.operacja = int(result[2])
+        self.odpowiedz = result[3]
+        self.ack = result[4]
+        self.stworz_pakiet()
+        print('Odebrano: '+self.final_packet)
+
+
+    def send_packet(self):
+        self.czas = time.ctime(time.time())
+        self.stworz_pakiet()
+        self.s.sendto(self.final_packet.encode('utf-8'), self.server)  # send to server
+        print('Wysłano: '+self.final_packet)
+
+    def proceed(self):
+        self.send_packet()
+        self.decode_packet()
+        self.decode_packet()
+        self.ack = "Potwierdzenie"
+        self.send_packet()
+        self.ack = ""
+
+
+    def proceed1(self):
+        try:
+            self.L = int(input("Podaj L: "))
+        except:
+            print("Podałeś nieprawidłową wartość!")
+            self.L = input("Podaj L: ")
+        self.operacja = self.L
+        self.ack = ""
+        self.send_packet()
+        self.decode_packet()
+        self.decode_packet()
+        self.ack = "Potwierdzenie"
+        self.send_packet()
+        self.ack = ""
+        self.max = self.operacja
+        print("Masz {} prób/y \n Wylosowana liczb jest z przedziału <1;9>".format(self.operacja))
+
+    def guess(self):
+        try:
+            self.odpowiedz = int(input("Zgadnij liczbę: "))
+        except:
+            print("Podałeś nieprawidłową wartość!")
+            self.odpowiedz= int(input("Zgadnij liczbę: "))
+        self.odpowiedz = str(self.odpowiedz)
+        self.proceed()
+        if(self.odpowiedz == 'Win'):
+            print("Wygrałeś!!! :)")
+            self.max = 0
+        elif(self.odpowiedz == "Opponent first"):
+            print("Przeciwnik był pierwszy :(")
+            self.czy_ktos_zgadl = True
+        elif(self.odpowiedz == "Wrong"):
+            print("Błedna odpowiedź")
+
+def Main():
+    p = packet()
+    p.proceed()
+    p.proceed1()
+    while p.max != 0:
+        if(p.czy_ktos_zgadl):
+            break;
+        p.max -= 1
+        p.guess()
+
+
+
+if __name__ == '__main__':
+    Main()
---
+++ server_o.py
@@ -0,0 +1,146 @@
+import socket
+import random
+import time
+import re
+
+class manage_socket:
+    host = '127.0.0.1' #local address
+    port = 5678 #random port > 1023
+    #-----------------------------------------------------------------------------
+    id = []
+    id_pom = 1
+    addr = ''
+    addr_pom = []
+    L = []
+    number_of_tries = 0
+    __secret_number = 0
+    czy_ktos_zgadl = False
+    bool_pom = False
+
+    operacja = 0
+    idd = ''
+    czas = ''
+    odpowiedz = ''
+    final_packet = ''
+    ack = ''
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #socket family and type, Datagram -> UDP packet style
+    s.bind((host, port))
+
+
+    def clear(self):
+        self.operacja = 0
+        self.ack = ''
+        self.idd = ''
+        self.czas = ''
+        self.odpowiedz = ''
+        self.id.clear()
+        self.id_pom = 1
+        self.addr = ''
+        self.addr_pom.clear()
+        self.L.clear()
+        self.number_of_tries = 0
+        self.__secret_number = 0
+        self.czy_ktos_zgadl = False
+
+
+    def stworz_pakiet(self):
+        self.final_packet = "Czas>"+self.czas+"<Identyfikator>"+self.idd+"<Operacja>"+str(self.operacja)+"<Odpowiedz>"+self.odpowiedz+"<Ack>"+self.ack+"<"
+
+    def decode_packet(self):
+        data, self.addr = self.s.recvfrom(1024)
+        data = data.decode('utf-8')
+        result = re.findall('>(.*?)<', data)
+        self.czas = result[0]
+        self.idd = result[1]
+        self.operacja = int(result[2])
+        self.odpowiedz = result[3]
+        self.ack = result[4]
+        self.stworz_pakiet()
+        print('Odebrano: '+self.final_packet)
+
+
+    def send_packet(self):
+        self.czas = time.ctime(time.time())
+        self.stworz_pakiet()
+        self.s.sendto(self.final_packet.encode('utf-8'), self.addr)  # send to server
+        print('Wysłano: '+self.final_packet)
+
+
+    def give_id(self):
+        self.decode_packet()#grabbing data and address form socket, waiting for UDP packets
+        self.ack = "Potwierdzenie"
+        self.send_packet()
+        self.ack = ""
+        self.id.append(str(self.id_pom))
+        self.addr_pom.append(self.addr)
+        self.idd = self.id[self.id_pom-1]
+        self.id_pom += 1
+        self.send_packet()
+        self.decode_packet()
+
+    def getL(self):
+        if(self.addr_pom == ''):
+            self.addr_pom = self.addr
+        self.decode_packet()
+        self.ack = "Potwierdzenie"
+        self.send_packet()
+        self.ack = ""
+        self.L.append(self.operacja)
+
+    def give_number_of_tries(self):
+        self.__secret_number = random.randint(1, 9)
+        print("Secret number: "+str(self.__secret_number))
+        self.number_of_tries = int((self.L[0] + self.L[1]) / 2)
+        self.operacja = self.number_of_tries
+        self.addr = self.addr_pom[0]
+        self.idd = "1"
+        self.send_packet()
+        self.decode_packet()
+        self.addr = self.addr_pom[1]
+        self.idd = "2"
+        self.ack = ""
+        self.send_packet()
+        self.decode_packet()
+
+    def check_number(self):
+        self.decode_packet()
+        pom_int = int(self.odpowiedz)
+        self.ack = "Potwierdzenie"
+        self.send_packet()
+        self.ack = ""
+        if not(self.czy_ktos_zgadl):
+            if(pom_int == self.__secret_number):
+                self.odpowiedz = 'Win'
+                self.czy_ktos_zgadl = True
+            else:
+                self.odpowiedz = 'Wrong'
+        else:
+            self.odpowiedz = "Opponent first"
+        self.send_packet()
+        self.decode_packet()
+
+
+
+
+def Main():
+    print("Server started")
+    m = manage_socket()
+    while True:
+        m.bool_pom = False
+        m.give_id()
+        m.give_id()
+        m.getL()
+        m.getL()
+        m.give_number_of_tries()
+        while True:
+            m.check_number()
+            if(m.bool_pom):
+                m.clear()
+                print("Server rebooted")
+                break
+            if(m.czy_ktos_zgadl):
+                m.bool_pom = True
+
+if __name__ == '__main__':
+    Main()
